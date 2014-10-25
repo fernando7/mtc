958ddc023ab8fffb5e0b536f9e32835c4ee5d561
fernando <fernando@Fernando-NB>
2014-10-25 20:24:25 -0200

    Commit

---
+++ src-test/br/com/fasam/mtc/AppTest.java
@@ -0,0 +1,15 @@
+package br.com.fasam.mtc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	
+	@Test
+	public void test() {
+		assertEquals("Teste", "Teste");
+	}
+
+}
---
+++ src/br/com/fasam/mtc/App.java
@@ -0,0 +1,8 @@
+package br.com.fasam.mtc;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Trabalho MTC. Integração Continua...");
+	}
+}
