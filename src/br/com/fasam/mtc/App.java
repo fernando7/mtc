e282c6d13942036512b00fa42de39ec14c11e4d5
fernando <fernando@Fernando-NB>
2014-10-25 20:30:48 -0200

    arquivo build

--- src/br/com/fasam/mtc/App.java
+++ src/br/com/fasam/mtc/App.java
@@ -3,6 +3,6 @@ package br.com.fasam.mtc;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Trabalho MTC. Integração Continua...");
+		System.out.println("Trabalho MTC. Commit Inicial");
 	}
 }
