608b21d3a1243e4975d55b58fae52cca45c2547e
fernando <fernando@Fernando-NB>
2014-10-25 16:34:38 -0200

    ......

--- integracao-continua/src/br/com/fasam/mtc/App.java
+++ integracao-continua/src/br/com/fasam/mtc/App.java
@@ -3,6 +3,6 @@ package br.com.fasam.mtc;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Trabalho MTC. Integração Continua. Jenkins");
+		System.out.println("Trabalho MTC. Integração Continua......");
 	}
 }
