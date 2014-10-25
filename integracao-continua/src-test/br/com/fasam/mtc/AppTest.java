a10f5c3f08701b3318bf3c59d0f1410e834cb4b4
fernando <fernando@Fernando-NB>
2014-10-25 16:48:15 -0200

    teste unitário

---
+++ integracao-continua/src-test/br/com/fasam/mtc/AppTest.java
@@ -0,0 +1,14 @@
+package br.com.fasam.mtc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		assertEquals("Teste", "Teste");
+	}
+
+}
