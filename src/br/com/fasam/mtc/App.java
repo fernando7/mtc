8b9dd04b8ddeb0d50cfb9f72d2c47b56aeea8182
fernando <fernando@Fernando-NB>
2014-10-26 00:19:54 -0200

    Teste

--- src/br/com/fasam/mtc/App.java
+++ src/br/com/fasam/mtc/App.java
@@ -3,6 +3,6 @@ package br.com.fasam.mtc;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Trabalho MTC. Commit Inicial");
+		System.out.println("Trabalho MTC.");
 	}
 }
